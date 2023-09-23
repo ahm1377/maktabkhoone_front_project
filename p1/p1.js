2c6fe81fb5d0cf968f05e2884ff58e4aebdbc6d9
ahm1377 <72826776+ahm1377@users.noreply.github.com>
2023-09-23 22:27:24 +0330

    add image and slider but slider have problem

---
+++ p1/p1.js
@@ -0,0 +1,120 @@
+
+$(document).ready(function(){
+  $("a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    }
+  });
+});
+
+// let slideIndex = 0;
+// showSlides();
+
+// function showSlides() {
+//   let i;
+//   let slides = document.getElementsByClassName("mySlides");
+//   let dots = document.getElementsByClassName("dot");
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";  
+//   }
+//   slideIndex++;
+//   if (slideIndex > slides.length) {slideIndex = 1}    
+//   for (i = 0; i < dots.length; i++) {
+//     dots[i].className = dots[i].className.replace(" active", "");
+//   }
+//   slides[slideIndex-1].style.display = "block";  
+//   dots[slideIndex-1].className += " active";
+//   setTimeout(showSlides, 2000); // Change image every 2 seconds
+// }
+
+
+
+let slideIndex = 0;
+let isMouseInside = false;
+let timer;
+
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  
+  slideIndex++;
+  
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }    
+  
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  
+  // اگر موس وارد شده باشد، توقف تایمر
+  if (!isMouseInside) {
+    timer = setTimeout(showSlides, 2000); // Change image every 2 seconds
+  }
+}
+
+// وقتی موس وارد می‌شود
+function mouseEnter() {
+  isMouseInside = true;
+  clearTimeout(timer); // توقف تایمر
+}
+
+// وقتی موس از داخل خارج می‌شود
+function mouseLeave() {
+  isMouseInside = false;
+  showSlides(); // ادامه تایمر
+}
+
+// اتصال توابع به رویدادهای موس
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelector(".mySlides-container").addEventListener("mouseenter", mouseEnter);
+  document.querySelector(".mySlides-container").addEventListener("mouseleave", mouseLeave);
+});
+
+let slideIndex = 1; // تغییر در مقدار اولیه
+
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
