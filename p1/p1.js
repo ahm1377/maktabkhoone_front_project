16a2f20dfc6f607bf32f751af53711ec317f564a
ahm1377 <72826776+ahm1377@users.noreply.github.com>
2023-09-30 10:54:11 +0330

    NEED EDIT SLIDER

--- p1/p1.js
+++ p1/p1.js
@@ -1,120 +1,79 @@
-
 $(document).ready(function(){
+  // Add smooth scrolling to all links
   $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
-      event.preventDefault();
+      // Prevent default anchor click behavior
+      // event.preventDefault();
+
+      // Store hash
       var hash = this.hash;
+      var targetoffset = $(hash).offset().top - 60;
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
       $('html, body').animate({
-        scrollTop: $(hash).offset().top
+        scrollTop: targetoffset
       }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
         window.location.hash = hash;
       });
-    }
+    } // End if
   });
 });
 
-// let slideIndex = 0;
-// showSlides();
 
-// function showSlides() {
-//   let i;
-//   let slides = document.getElementsByClassName("mySlides");
-//   let dots = document.getElementsByClassName("dot");
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = "none";  
-//   }
-//   slideIndex++;
-//   if (slideIndex > slides.length) {slideIndex = 1}    
-//   for (i = 0; i < dots.length; i++) {
-//     dots[i].className = dots[i].className.replace(" active", "");
-//   }
-//   slides[slideIndex-1].style.display = "block";  
-//   dots[slideIndex-1].className += " active";
-//   setTimeout(showSlides, 2000); // Change image every 2 seconds
-// }
-
-
-
-let slideIndex = 0;
-let isMouseInside = false;
-let timer;
-
-showSlides();
-
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  
-  slideIndex++;
-  
-  if (slideIndex > slides.length) {
-    slideIndex = 1;
-  }    
-  
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  
-  // اگر موس وارد شده باشد، توقف تایمر
-  if (!isMouseInside) {
-    timer = setTimeout(showSlides, 2000); // Change image every 2 seconds
-  }
-}
-
-// وقتی موس وارد می‌شود
-function mouseEnter() {
-  isMouseInside = true;
-  clearTimeout(timer); // توقف تایمر
-}
-
-// وقتی موس از داخل خارج می‌شود
-function mouseLeave() {
-  isMouseInside = false;
-  showSlides(); // ادامه تایمر
-}
-
-// اتصال توابع به رویدادهای موس
-document.addEventListener("DOMContentLoaded", function () {
-  document.querySelector(".mySlides-container").addEventListener("mouseenter", mouseEnter);
-  document.querySelector(".mySlides-container").addEventListener("mouseleave", mouseLeave);
-});
-
-let slideIndex = 1; // تغییر در مقدار اولیه
-
+var slideIndex = 1;
+var timer;
 showSlides(slideIndex);
 
 function plusSlides(n) {
+  clearTimeout(timer);
   showSlides(slideIndex += n);
 }
 
 function currentSlide(n) {
+  clearTimeout(timer);
   showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
-  
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+    slides[i].style.display = "none";
   }
-  
   for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].style.backgroundColor = "#bbb";
   }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].style.backgroundColor = "#007bff";
   
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+  // تنظیم تایمر برای تغییر اسلاید هر 5 ثانیه
+  timer = setTimeout(function() { plusSlides(1); }, 5000);
 }
+
+// تنظیم تایمر اولیه
+timer = setTimeout(function() { plusSlides(1); }, 5000);
+
+// توقف تایمر در صورت رفتن موس روی اسلایدر
+var slideshowContainer = document.querySelector('.slideshow-container');
+slideshowContainer.addEventListener('mouseover', function() {
+  clearTimeout(timer);
+});
+
+// شروع تایمر در صورت خروج موس از اسلایدر
+slideshowContainer.addEventListener('mouseout', function() {
+  timer = setTimeout(function() { plusSlides(1); }, 5000);
+});
+//  هنگام کلیک بزرگتر شدن دکمه اسلاید شو
+$(".prev").on("click",(e)=>{
+  document.style.transform.scale(1.2);
+})
+$(".next").on("click",(e)=>{
+  document.style.transform.scale(1.2);
+})
